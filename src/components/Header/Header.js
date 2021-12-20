531476f7c1dd690dc0e8d95197ce28880294e9b2
Barry Hanna <barryahanna@gmail.com>
2021-12-20 12:11:02 +0000

    exercise 1: header for desktop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,19 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopActionGroup>
         <Logo />
+        <DesktopSubscribeCTA>
+          <Button>Subscribe</Button>
+          <a href="#">Already a subscriber?</a>
+        </DesktopSubscribeCTA>
       </MainHeader>
     </header>
   );
@@ -39,6 +51,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +75,44 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+    transform: translateY(5px);
+  }
+`;
+
+const DesktopSubscribeCTA = styled.div`
+  display: none;
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+    flex-direction: column;
+    gap: 0.6875rem;
+    padding: 1rem;
+
+    a {
+      font-style: italic;
+      font-size: 0.875rem;
+      text-align: center;
+      text-decoration: underline;
+      text-underline-offset: 4px;
+    }
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.desktopAndUp} {
+    align-items: baseline;
+    justify-content: space-between;
+  }
 `;
 
 export default Header;
